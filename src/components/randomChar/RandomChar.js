0543d04f6712d4d7d679320c962c15a23aab3335
Tedzury <asdrogachev@gmail.com>
2022-07-22 09:24:16 +0300

    second commit. Done with Char List, Char Info and Random Char

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,41 +1,115 @@
+import { Component } from 'react';
+import Spinner from '../spinner/spinner'
+import ErrorMessage from '../errorMessage/errorMessage';
+import MarvelService from '../../services/MarvelService';
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-const RandomChar = () => {
+class RandomChar extends Component {
+
+    state = {
+        char: {},
+        loading: true,
+        error: false,
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({char, loading: false})
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true,
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true,
+        })
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
+        this.marvelService 
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+
+    render() {
+
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
+        return (
+            <div className="randomchar">
+
+                {errorMessage}
+                {spinner}
+                {content}
+
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button className="button button__main">
+                        <div className="inner" onClick={this.updateChar}>try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+const View = ({char}) => {
+
+    let {name, description, thumbnail, homepage, wiki} = char;
+    let imgClassName = 'randomchar__img';
+
+    if (!description) {
+        description = 'К сожалению, у данного персонажа(ей) пока нет описания.'
+    } 
+    if (description.length > 200) {
+        description = description.slice(0, 200) + '...';  
+    }
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgClassName = 'randomchar__img__notfound'
+    }
+
     return (
-        <div className="randomchar">
             <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
+                <img src={thumbnail} alt="Random character" className={imgClassName}/>
                 <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
+                    <p className="randomchar__name">{name}</p>
                     <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                        {description}
                     </p>
                     <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
+                        <a href={homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href="#" className="button button__secondary">
+                        <a href={wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
                 </div>
-            </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-        </div>
+           </div>
     )
 }
 
